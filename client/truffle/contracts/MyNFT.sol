5def0345f636beaa0c540bddc5ff686f6c8e9827
lebrancconvas <lebrancconvas@gmail.com>
2021-12-28 21:37:22 +0700

    :sparkles: Create New Contract.

---
+++ client/truffle/contracts/MyNFT.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MyNFT is ERC721 {
+	using Counters for Counters.Counter; 
+	Counters.Counter private _tokenId;
+	construct() ERC721('TestNFT', 'TEST') {}
+	function mint(address owner) public {
+		_tokenId.increment();
+		uint newTokenID = _tokenId.current();
+		_mint(owner, newTokenID); 
+	}
+}
