379f14b64bd99101508625a792c924e1c72c0017
lebrancconvas <lebrancconvas@gmail.com>
2021-12-28 23:21:28 +0700

    :hammer: Add Contract Deployment.

--- client/truffle/migrations/1_initial_migration.js
+++ client/truffle/migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const MyNFT = artifacts.require("MyNFT");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(MyNFT); 
 };
